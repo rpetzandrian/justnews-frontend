01636d11a8b97f28ab7404fc1142f8888f7b5071
rpetzandrian <rpetzandrian@gmail.com>
2021-07-07 16:33:52 +0700

    service worker pusher beams

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react';
 import { SWRConfig } from 'swr';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/service-worker.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
   return (
     // <SWRConfig value={{ refreshInterval: 1000, fetcher: fetch }}>
     <Component {...pageProps} />
