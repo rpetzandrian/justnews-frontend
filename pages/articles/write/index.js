132fa4ded657d5954b43014f44d42ca9f115951e
rpetzandrian <rpetzandrian@gmail.com>
2021-07-07 16:34:03 +0700

    text editor

--- pages/articles/write/index.js
+++ pages/articles/write/index.js
@@ -1,34 +1,91 @@
 import Image from "next/image"
+import dynamic from 'next/dynamic'
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Card, Col, Form, Row, Button } from "react-bootstrap"
 import { useForm } from "react-hook-form"
 import Footer from "../../../components/Footer"
 import Navigation from "../../../components/Navigation"
 import { actionPosts, useAuth, useCategory } from "../../api"
+import 'react-quill/dist/quill.snow.css';
+import TurndownService from "turndown";
+const Quill = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+})
+const modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+}
+/*
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+  'video',
+]
 
 const WriteArticle = () => {
+  const turndownService = new TurndownService()
   const router = useRouter()
   const { auth, loggedOut, mutateAuth, loadingAuth } = useAuth()
   const { category } = useCategory()
   const { register, handleSubmit, formState: { errors } } = useForm();
   const [cover, setCover] = useState(null)
+  const [value, setValue] = useState('')
 
   useEffect(() => {
     if (!loadingAuth && loggedOut) router.replace('/')
   }, [auth, loadingAuth])
 
+  const handleChange = value => {
+    setTimeout(() => {
+      const markdown = turndownService.turndown(value);
+      setValue(markdown)
+    })
+  }
+
   const add = (data) => {
+    console.log(value)
     const formData = new FormData()
     data.title ? formData.append('title', data.title) : ''
     data.category_id ? formData.append('category_id', data.category_id) : ''
-    data.text ? formData.append('text', data.text) : ''
+    // data.text ? formData.append('text', data.text) : ''
+    value ? formData.append('text', value) : ''
     cover ? formData.append('cover', cover) : ''
     auth?.data?.id ? formData.append('user_id', auth?.data?.id) : ''
 
     actionPosts.addPost(formData)
   }
-  console.log(auth)
+
   return (
     <>
       <Navigation />
@@ -76,18 +133,18 @@ const WriteArticle = () => {
                   </Form.Group>
                 </Col>
               </Row>
-              <Col xs={9} className='d-flex justify-content-start my-2'>
+              {/* <Col xs={9} className='d-flex justify-content-start my-2'>
                 <p className='pt-3'>Attachment :</p>
                 <img src='/icons/Image.svg' alt='image' className='mx-2' />
                 <img src='/icons/Play.svg' alt='video' className='mx-2' />
                 <img src='/icons/Align.svg' alt='align' className='mx-2' />
                 <img src='/icons/Link.svg' alt='link' className='mx-2' />
                 <img src='/icons/Scale.svg' alt='text' className='mx-2' />
-              </Col>
+              </Col> */}
               <Col xs={12}>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                  <Form.Control placeholder='Type the Article' as="textarea" rows={20} className='py-3 rounded upload-box' {...register('text')} />
-                </Form.Group>
+                <Quill className='upload-box mt-3' modules={modules} placeholder='Insert content here...' formats={formats} theme="snow" onChange={(e) => handleChange(e)}>
+                  <div className='overflow-auto' style={{ minHeight: '50vh', maxHeight: '51vh' }} ></div>
+                </Quill>
               </Col>
             </Form>
             <Button variant='primary' className='w-100 text-center mt-3 py-3 rounded' onClick={handleSubmit(add)}>
